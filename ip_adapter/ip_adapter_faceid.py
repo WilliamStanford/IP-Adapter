99581389ac8bbc60e8b509abe5e7df1bb3c2f417
WilliamStanford <54320229+WilliamStanford@users.noreply.github.com>
2024-01-04 14:38:33 -0500

    Update ip_adapter_faceid.py

--- ip_adapter/ip_adapter_faceid.py
+++ ip_adapter/ip_adapter_faceid.py
@@ -207,6 +207,8 @@ class IPAdapterFaceID:
         **kwargs,
     ):
         self.set_scale(scale)
+        if prompt_embeds is not None:
+            print(prompt_embeds)
 
        
         num_prompts = faceid_embeds.size(0)
